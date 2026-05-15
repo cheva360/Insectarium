b4faa6d0b9a071339f5ed0cdc3d123528952d858
taxiLotus <spacepenguin2323@gmail.com>
2026-05-14 20:42:20 -0700

    This reverts commit f705d2ffdd0bce2bfde6342e8a5398edf55dc593, reversing changes made to 709f7eef4bb589c40e533c8cb2e1e7d17eb173ec.

--- Assets/Grayboxing/TutorialDoor.cs
+++ Assets/Grayboxing/TutorialDoor.cs
@@ -1,4 +1,3 @@
-using System.Collections;
 using UnityEngine;
 
 public class TutorialDoor : MonoBehaviour
@@ -7,26 +6,15 @@ public class TutorialDoor : MonoBehaviour
 
     [SerializeField] private GameObject TutorialTape;
 
-    // Update is called once per frame
-    void Update()
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
     {
-        if (TutorialTape == null)
-        {
-            StartCoroutine(CountDown());//countdown before door opens
-            
-        }
         
     }
 
-    IEnumerator CountDown()
+    // Update is called once per frame
+    void Update()
     {
-        yield return new WaitForSeconds (5.0f);
-        OpenDoor();
-
-    }
-
-    private void OpenDoor()//open the door!!! to the main level
-    {
-        gameObject.SetActive(false);
+        if (TutorialTape = null) this.gameObject.SetActive(false);
     }
 }
